746641df87f49c0edc9a2e08d265dd3644155db5
Sailendra Darbha <sailendra.darbha@gmail.com>
2022-03-23 14:40:22 +0530

    pls deploy im crying now

--- ims/manage.py
+++ ims/manage.py
@@ -8,11 +8,12 @@ import pathlib
 
 def main():
     """Run administrative tasks."""
-    DOT_ENV_PATH = pathlib.Path()/'.env'
-    if DOT_ENV_PATH.exists():
-        dotenv.read_dotenv(str(DOT_ENV_PATH))
-    else:
-        print('no env file found')
+    # DOT_ENV_PATH = pathlib.Path()/'.env'
+    # if DOT_ENV_PATH.exists():
+    #     dotenv.read_dotenv(str(DOT_ENV_PATH))
+    # else:
+    #     print('no env file found')
+    dotenv.read_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ims.settings')
     try:
         from django.core.management import execute_from_command_line
